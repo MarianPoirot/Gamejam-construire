bd7a0c44b88ecc912e2f329cd4b5b8e7b70c9e89
MarianPoirot <81marianp@gmail.com>
2023-03-04 18:02:38 +0100

    restart button

--- Scripts/World/Ending.gd
+++ Scripts/World/Ending.gd
@@ -1,15 +1,9 @@
 extends Control
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
-	pass
-
+func _physics_process(_delta):
+	if Input.is_action_pressed("restart"):
+		_on_button_button_down()
 
 func _on_button_button_down():
-	pass
+	if get_tree().change_scene_to_file("res://Scenes/World/Main.tscn") != OK:
+		print ("Error passing from Opening scene to main scene")
