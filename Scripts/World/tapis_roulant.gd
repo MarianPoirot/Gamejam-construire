2deaaa39c458b610b3e0da092b9365c7a081520e
Martin Rouault <mrouault@hotmail.fr>
2023-03-04 21:50:13 +0100

    Tapis plus grand

--- Scripts/World/tapis_roulant.gd
+++ Scripts/World/tapis_roulant.gd
@@ -13,7 +13,6 @@ func _process(delta):
 		monter()
 
 func deplacer(sens:int):
-	print("auie")
 	for c in get_overlapping_areas():
 		c.position.y += sens * vitesse
 
@@ -29,4 +28,4 @@ func _on_haut_area_entered(area):
 
 
 func _on_bas_area_entered(area):
-	area.position.y-=($CollisionShape2D.shape.size.y-50)*scale.y
+	area.position.y-=($CollisionShape2D.shape.size.y-200)*scale.y
