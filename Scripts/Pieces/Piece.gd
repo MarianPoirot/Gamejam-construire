b6a182a87777dfb9fa2be287a5d28a5e9e22812d
MarianPoirot <81marianp@gmail.com>
2023-03-04 12:44:46 +0100

    coques sup et inf fonctionnelle avec correction du snap

--- Scripts/Pieces/Piece.gd
+++ Scripts/Pieces/Piece.gd
@@ -5,20 +5,17 @@ var can_grab = true
 var grabbed = false
 var grabbed_offset = Vector2()
 
-func _ready():
-	pass # Replace with function body.
-
 func _on_input_event(_viewport, event, _shape_idx):
 	if event.is_action_pressed("pick") and can_grab:
 		grabbed = true
-		grabbed_offset = position - get_global_mouse_position()
+		grabbed_offset = global_position - get_global_mouse_position()
 	if event.is_action_released("pick"):
 		grabbed = false
 		try_snap()
 
 func _process(_delta):
 	if Input.is_action_pressed("pick") and grabbed:
-		position = get_global_mouse_position() + grabbed_offset
+		global_position = get_global_mouse_position() + grabbed_offset
 
 func try_snap():
 	if not depandancies_metted():
@@ -29,11 +26,11 @@ func try_snap():
 			snap(area)
 
 func depandancies_metted():
-	return Id_pieces.dependancies[id].all(func (dep): return Id_pieces.state_dependencies[id])
+	return Id_pieces.dependancies[id].all(func (dep): return Id_pieces.state_dependencies[dep])
 
 func snap(area):
 	can_grab = false
-	self.position=area.position-$Ancre.position
+	self.global_position=area.global_position-$Ancre.position*scale.x
 	for ancre in $recepteurAncre.get_children():
 		ancre.activate()
 	
