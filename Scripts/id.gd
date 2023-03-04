f232018503c449d3b8a14a8d51b5ad219fcff973
MarianPoirot <81marianp@gmail.com>
2023-03-04 09:24:04 +0100

    pieces ancre

---
+++ Scripts/id.gd
@@ -0,0 +1,19 @@
+extends Node
+
+enum id_pieces{
+	ancre,
+	ballon,
+	bouchon,
+	cable,
+	chaudiere,
+	conduitVapeur,
+	coque,
+	diode,
+	eau,
+	entonnoir,
+	ferAsouder,
+	moteur,
+	piques,
+	sodium,
+	turbine
+}
