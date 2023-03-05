3c859467398d8af2f7421c99ac782275dc1a744e
MarianPoirot <81marianp@gmail.com>
2023-03-05 10:19:51 +0100

    explosion!

--- Scripts/World/Main.gd
+++ Scripts/World/Main.gd
@@ -1,8 +1,12 @@
 extends Node2D
-
+	
 func _on_meche_just_snapped():
 	$Explosion.play()
+	$Timer.start()
+	await $Timer.timeout
+	$explosion.emitting=true
 
 func _on_audio_stream_player_finished():
+	$explosion.emitting=false
 	if get_tree().change_scene_to_file("res://Scenes/World/Ending.tscn") != OK:
 		print ("Error passing from Opening scene to ending scene")
