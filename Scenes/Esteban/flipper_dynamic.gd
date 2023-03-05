5b128d36ce5b5f971042577ab802816f819f0b80
Zebedee-balistique <esteban.meleroz@gmail.com>
2023-03-05 12:24:01 +0100

    Ajout flipper
    J'ai mis un début de pinball

---
+++ Scenes/Esteban/flipper_dynamic.gd
@@ -0,0 +1,20 @@
+extends CharacterBody2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if Input.is_action_just_pressed("Flipper_droit"):
+		position.y = position.y - 60
+	if Input.is_action_pressed("Flipper_droit"):
+		if rotation_degrees > -30:
+			rotation += -50 * delta
+	if Input.is_action_just_released("Flipper_droit"):
+		set_rotation_degrees(0)
+		position.y = position.y + 60
+		
+
--- Scenes/World/Button_bob_omb.gd
+++ Scenes/World/Button_bob_omb.gd
@@ -1,6 +1,8 @@
 extends Button
 
 var bob_omb = load("res://Scenes/Esteban/bob_omb.tscn")
+var flipper = load("res://Scenes/Esteban/flipper.tscn")
+var flipper_exist = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -9,6 +11,11 @@ func spawn():
 	var new_bob_omb = bob_omb.instantiate()
 	new_bob_omb.position = Vector2(-300,-500)
 	self.add_child(new_bob_omb)
+	if not flipper_exist:
+		flipper_exist = true
+		var flipper_droit = flipper.instantiate()
+		flipper_droit.position = Vector2(-400, -50)
+		self.add_child(flipper_droit)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
@@ -16,4 +23,6 @@ func _process(_delta):
 
 
 func _on_button_down():
+	print("ça marche")
 	spawn()
+
